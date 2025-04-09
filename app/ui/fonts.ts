96b610517a297e45abb52915d36c1124964c6b70
YuriiDzoma <hp_Yr8xoCiyd9HTYeklWGhXjSpbSU60xh4BiJoc>
2025-04-09 13:35:48 +0300

    33

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,7 @@
 import { Inter } from 'next/font/google';
-import { Roboto } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const roboto = Roboto({
-    subsets: ['latin'],
-    weight: ['400', '700'], // вкажи потрібну вагу
-});
 
 export const lusitana = Lusitana({
     weight: ['400', '700'],
