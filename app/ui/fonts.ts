dbced0cca74b992e500992179d17ce6920780a1d
YuriiDzoma <hp_Yr8xoCiyd9HTYeklWGhXjSpbSU60xh4BiJoc>
2025-04-09 13:22:56 +0300

    fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,10 @@ import { Roboto } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const roboto = Roboto({weight: '400'});
+export const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '700'], // вкажи потрібну вагу
+});
 
 export const lusitana = Lusitana({
     weight: ['400', '700'],
